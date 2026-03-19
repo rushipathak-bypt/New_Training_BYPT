c3b1bdbecc29fd3ad1918c538f738be25b733b67
rushipathak-bypt <rushi.pathak@bypeopletechnologies.com>
2026-03-19 17:37:43 +0530

    Completed tasks of Day-1
    Understood pyenv usage and difference between venv and conda and also used pip, pip freeze to create requirement.txt file
    
    Installed Black and Flake8 extensions in VS Code and utilized them by messy code in messy.py
    
    Also understood difference in running .py file vs running -m module and PEP8 coding style
    
    Completed Practical Tasks by
    -creating virtual env,
    -installed fastapi, uvicorn, pandas, requests, black, flake8
    -built virtual env again from requirement.txt file after deleting virtual env

--- Day-1/messy.py
+++ Day-1/messy.py
@@ -1,1 +1,16 @@
-print("hello")
+import pandas as pd,requests
+
+def   myFunction( ):
+ print("starting")
+ x=1+2
+ y=  3+4
+ if(x<y):
+  print( "x is smaller" )
+ else:
+     print("y is smaller")
+ data ={"name":"Rushi","age":20}
+ df=pd.DataFrame(data,index=[0])
+ print( df )
+ unused_var=10
+ for  i in range( 5 ):
+  print(i)
